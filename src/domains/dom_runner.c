237421de0a2b850739ba60b5bc7cfcee9572a0c7
Dmytro Firsov <dmytro_firsov@epam.com>
2024-05-13 11:27:09 +0300

    domains: introduce Domain-U creation
    Add domain configuration and creation logic for Domain-U in
    AoS application. It is controlled by previously introduced option
    CONFIG_AOS_ENABLE_DOMU and supports backend configuration for network
    and pvblock, which are currently assigned to domain. Domain-U uses
    partition on UFS as rootfs, so it is needed to create proper image
    during build.
    
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- src/domains/dom_runner.c
+++ src/domains/dom_runner.c
@@ -8,6 +8,10 @@
 #include <xen_dom_mgmt.h>
 #include <zephyr/logging/log.h>
 
+#ifdef CONFIG_AOS_ENABLE_DOMU
+#include "domu/domu_runner.h"
+#endif
+
 LOG_MODULE_REGISTER(domains);
 
 extern struct xen_domain_cfg domd_cfg;
@@ -19,7 +23,15 @@ int create_domains(void)
     rc = domain_create(&domd_cfg, 1);
     if (rc) {
         LOG_ERR("Failed to start Domain-D, rc = %d", rc);
+        return rc;
     }
+
+#ifdef CONFIG_AOS_ENABLE_DOMU
+    rc = domu_start();
+    if (rc) {
+        LOG_ERR("Failed to start Domain-U, rc = %d", rc);
+    }
+#endif
 
     return rc;
 }
